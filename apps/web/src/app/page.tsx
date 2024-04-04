1618c322fcec5a2ef1a49ca907dfc4e67d35e9e3
Ifeoluwa Odubela <i.odubela@gmail.com>
2024-04-04 09:53:06 +0100

    fix build issues

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,4 +1,4 @@
-import { BackgroundGradient } from "@repo/ui/background-gradient";
+import { BackgroundGradient } from "../components/background-gradient";
 
 function Gradient({
   conic,
@@ -20,21 +20,31 @@ function Gradient({
 
 export default function Page(): JSX.Element {
   return (
-    <main className="flex flex-col items-center justify-center min-h-screen p-24 gap-6">
-      <h1>Open Heavens Reader</h1>
-      <div>
-        <BackgroundGradient className="rounded-[22px] max-w-sm p-4 sm:p-10 bg-white dark:bg-zinc-900">
-          <p className="text-base sm:text-xl text-black mt-4 mb-2 dark:text-neutral-200">
+    <main className="flex flex-col items-center justify-center min-h-screen p-8 md:p-24 gap-6">
+      <h1 className="text-3xl">Open Heavens Reader</h1>
+      <div className="relative">
+        <BackgroundGradient className="rounded-[22px] max-w-sm p-4 sm:p-10 bg-white dark:bg-zinc-900 flex flex-col gap-3">
+          <p className="text-base sm:text-xl text-black mt-4 mb-2 dark:text-neutral-200 text-center sm:text-left">
             A simple reader for the Open Heavens devotional
           </p>
-          <p className=" text-neutral-600 dark:text-neutral-400 text-balance leading-relaxed">
-            Is it possible to have a simplified reader for the RCCG Open Heavens devotional? Something
-            that is comfortable and easy to read where you won't need to strain your
-            eyes. A similar reading experience to the <a className="dark:text-amber-600" target="_" href="https://bible.com"> bible app</a>. <span className="uppercase font-medium">WITHOUT THE ADS</span>
+          <p className=" text-neutral-600 dark:text-neutral-400 text-balance leading-relaxed text-center sm:text-left">
+            Is it possible to have a simplified reader for the RCCG Open Heavens
+            devotional? Something that is comfortable and easy to read where you
+            won't need to strain your eyes. A similar reading experience to the{" "}
+            <a
+              className="dark:text-amber-600"
+              target="_"
+              href="https://bible.com"
+            >
+              bible app
+            </a>
+            . <span className="uppercase font-medium">WITHOUT THE ADS</span>
           </p>
-          <button className="rounded-full uppercase px-3 py-1 text-white flex items-center space-x-1 bg-black mt-4 text-xs font-bold dark:bg-zinc-800">
-            <span>Coming Soon</span>
-          </button>
+          <div className="w-full">
+            <button className="rounded-full mx-auto sm:m-0 uppercase px-3 py-1 text-white flex items-center space-x-1 bg-black mt-4 text-xs font-bold dark:bg-zinc-800">
+              <span>Coming Soon</span>
+            </button>
+          </div>
         </BackgroundGradient>
       </div>
     </main>
