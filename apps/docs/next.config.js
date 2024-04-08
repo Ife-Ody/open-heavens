63ad90572147f41dccdfe171aa8ef54159f6695a
Ifeoluwa Odubela <i.odubela@gmail.com>
2024-04-08 12:04:26 +0100

    removed unused docs

--- apps/docs/next.config.js
+++
@@ -1,6 +0,0 @@
-const { withContentlayer } = require('next-contentlayer')
-/** @type {import('next').NextConfig} */
-
-const nextConfig = { reactStrictMode: true, swcMinify: true, transpilePackages: ["@repo/ui"], }
-
-module.exports = withContentlayer(nextConfig)
