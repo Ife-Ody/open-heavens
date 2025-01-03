3b5cda3a64949af2b08bbd73d6491b9dc27ef690
Ifeoluwa Odubela <i.odubela@gmail.com>
2025-01-03 23:58:36 +0000

    improve seo

--- apps/web/src/components/post-template.tsx
+++ apps/web/src/components/post-template.tsx
@@ -15,39 +15,59 @@ export function PostTemplate({ post }: { post: Post }): JSX.Element {
   if (!post) return <EmptyPost />;
 
   return (
-    <div className="flex flex-col gap-3 whitespace-wrap">
-      <h1 className={cn(`text-[${fontSize * 1.5}px] font-semibold`)}>
-        {titleCase(post.title)}
-      </h1>
-      <h2 className={cn(`text-[${fontSize + 2}px] font-bold uppercase`)}>
-        Memory Verse
-      </h2>
-      <blockquote className="p-3 pl-6 italic border-l-2 text-muted-foreground">
-        {post.memorizeText}
-      </blockquote>
-      <p className={cn("font-semibold text-primary", `text-[${fontSize}px]`)}>
-        {post.memorizeVerse} (KJV)
-      </p>
-      <h2 className={cn(`text-[${fontSize + 2}px] font-bold uppercase`)}>
-        Read
-      </h2>
-      <div className={cn("font-semibold text-primary", `text-[${fontSize}px]`)}>
-        <BibleReference content={post.read} />
-      </div>
-      <h2 className={cn(`text-[${fontSize + 2}px] font-bold uppercase`)}>
-        Message
-      </h2>
-      <div
-        className={cn(
-          "leading-7 text-justify whitespace-normal break-words",
-          `text-[${fontSize}px]`,
-        )}
-      >
-        {/* <BibleReference content={post.bodyText} /> */}
-        {parse(post.bodyText)}
-      </div>
-      <div>
-        {post.pointHeader && (
+    <article className="flex flex-col gap-3 whitespace-wrap">
+      <header>
+        <h1
+          className={cn(`text-[${fontSize * 1.5}px] font-semibold`)}
+          itemProp="headline"
+        >
+          {titleCase(post.title)}
+        </h1>
+        <meta
+          itemProp="datePublished"
+          content={new Date(post.date).toISOString()}
+        />
+      </header>
+
+      <section aria-label="Memory Verse">
+        <h2 className={cn(`text-[${fontSize + 2}px] font-bold uppercase`)}>
+          Memory Verse
+        </h2>
+        <blockquote className="p-3 pl-6 italic border-l-2 text-muted-foreground">
+          {post.memorizeText}
+        </blockquote>
+        <p className={cn("font-semibold text-primary", `text-[${fontSize}px]`)}>
+          <cite>{post.memorizeVerse}</cite> (KJV)
+        </p>
+      </section>
+
+      <section aria-label="Bible Reading">
+        <h2 className={cn(`text-[${fontSize + 2}px] font-bold uppercase`)}>
+          Read
+        </h2>
+        <div
+          className={cn("font-semibold text-primary", `text-[${fontSize}px]`)}
+        >
+          <BibleReference content={post.read} />
+        </div>
+      </section>
+
+      <section aria-label="Main Message" itemProp="articleBody">
+        <h2 className={cn(`text-[${fontSize + 2}px] font-bold uppercase`)}>
+          Message
+        </h2>
+        <div
+          className={cn(
+            "leading-7 text-justify whitespace-normal break-words",
+            `text-[${fontSize}px]`,
+          )}
+        >
+          {parse(post.bodyText)}
+        </div>
+      </section>
+
+      {post.pointHeader && (
+        <section aria-label="Key Points">
           <h2
             className={cn(
               `text-[${fontSize + 2}px] font-bold uppercase leading-0`,
@@ -55,31 +75,27 @@ export function PostTemplate({ post }: { post: Post }): JSX.Element {
           >
             {post.pointHeader}
           </h2>
-        )}
-        {post.pointText && (
-          <p className={cn("leading-7 text-justify", `text-[${fontSize}px]`)}>
-            {post.pointText}
-          </p>
-        )}
-      </div>
-      <div>
-        {post.bibleInOneYear && (
-          <>
-            <h2 className={cn(`text-[${fontSize + 2}px] font-bold uppercase`)}>
-              Bible in one Year
-            </h2>
-            <div
-              className={cn(
-                "font-semibold text-primary",
-                `text-[${fontSize}px]`,
-              )}
-            >
-              <BibleReference content={post.bibleInOneYear} />
-            </div>
-          </>
-        )}
-      </div>
-    </div>
+          {post.pointText && (
+            <p className={cn("leading-7 text-justify", `text-[${fontSize}px]`)}>
+              {post.pointText}
+            </p>
+          )}
+        </section>
+      )}
+
+      {post.bibleInOneYear && (
+        <section aria-label="Bible in One Year">
+          <h2 className={cn(`text-[${fontSize + 2}px] font-bold uppercase`)}>
+            Bible in one Year
+          </h2>
+          <div
+            className={cn("font-semibold text-primary", `text-[${fontSize}px]`)}
+          >
+            <BibleReference content={post.bibleInOneYear} />
+          </div>
+        </section>
+      )}
+    </article>
   );
 }
 
