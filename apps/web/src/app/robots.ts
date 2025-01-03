716afabc9042e64b87ceaaab0437a384bfc17553
Ifeoluwa Odubela <i.odubela@gmail.com>
2025-01-03 23:54:54 +0000

    fix robots.txt

--- apps/web/src/app/robots.ts
+++ apps/web/src/app/robots.ts
@@ -5,7 +5,6 @@ export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
       userAgent: "*",
-      allow: "/",
       disallow: "/private/",
     },
     sitemap: `${HOME_DOMAIN}/sitemap.xml`,
