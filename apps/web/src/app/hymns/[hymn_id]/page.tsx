29507f7df51765c33e186cfdda761e8d344b34d4
Ifeoluwa Odubela <i.odubela@gmail.com>
2025-04-08 22:25:24 +0100

    fix hymns titles

--- apps/web/src/app/hymns/[hymn_id]/page.tsx
+++ apps/web/src/app/hymns/[hymn_id]/page.tsx
@@ -10,8 +10,11 @@ export const generateMetadata = async ({
   const { hymn_id } = await params;
   const hymn = hymns.find((hymn) => hymn.id === parseInt(hymn_id));
   return constructMetadata({
-    title: `Hymn ${hymn?.hymn_number} - ${hymn?.title} - Open Heavens Hymn`,
-    description: `${truncate(hymn?.lyrics, 160)}...`,
+    title: truncate(
+      `Hymn ${hymn?.hymn_number} - ${hymn?.title} - Open Heavens Hymn`,
+      60,
+    ) as string,
+    description: truncate(hymn?.lyrics, 160) as string,
   });
 };
 
